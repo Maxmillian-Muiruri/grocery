df3cdeea39fb72c3b0352de969d9b4495adb666c
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2024-05-24 19:56:33 +0300

    add functions

--- index.js
+++ index.js
@@ -20,3 +20,62 @@ form.addEventListener("submit", addItem);
 clearBtn.addEventListener("click", clearItems);
 // display items onload
 window.addEventListener("DOMContentLoaded", setupItems);
+
+//functions
+
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `<p class="title">${value}</p>
+              <div class="btn-container">
+                <!-- edit btn -->
+                <button type="button" class="edit-btn">
+                  <i class="fas fa-edit"></i>
+                </button>
+                <!-- delete btn -->
+                <button type="button" class="delete-btn">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </div>
+            `;
+    // add event listeners to both buttons;
+
+    const deleteBtn = element.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    const editBtn = element.querySelector(".edit-btn");
+    editBtn.addEventListener("click", editItem);
+
+    // append child
+
+    list.appendChild(element);
+    // display alert
+
+    displayAlert("item added to the list", "success");
+    // show container
+
+    container.classList.add("show-container");
+    // set local storage
+
+    addToLocalStorage(id, value);
+    // set back to default
+
+    setBackToDefault();
+  } else if (value !== "" && editFlag) {
+    editElement.innerHTML = value;
+    displayAlert("value changed", "success");
+
+    // edit  local storage
+    editLocalStorage(editID, value);
+    setBackToDefault();
+  } else {
+    displayAlert("please enter value", "danger");
+  }
+}
